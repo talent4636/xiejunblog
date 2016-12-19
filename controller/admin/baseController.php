3c1ec401078b0770dc99702d951a489b1b37cde9
talent4636@gmail.com <talent4636@gmail.com>
2016-12-20 01:40:58 +0800

    1. 后台登陆，页面&功能； 2. 前台跳后台，后台跳前台超链接； 3. 删除文章功能； 4. 后台加入基本样式； 5. 后台加入标题和没有文章时候的样式

--- controller/admin/baseController.php
+++ controller/admin/baseController.php
@@ -3,25 +3,47 @@
 class controller_admin_baseController extends controller_baseController{
 
     public function __construct(){
+        session_start();
         parent::__construct();
-        if(!self::checkAdmin()){
+        if($_SERVER['PATH_INFO'] != '/admin-post_login' && !self::checkAdmin()){
             self::login();
         }
     }
 
     public function checkAdmin(){
-        return true;
+        if(isset($_SESSION['admin'])){
+            if($_SESSION['admin']==md5('xiejun')){
+                return true;
+            }
+        }
+        return false;
     }
 
     public function login(){
-        //TODO
+        if(self::checkAdmin()){
+            self::index();
+            exit;
+        }
+        $this->view->display('admin/login.html');
+        exit;
     }
 
     public function post_login(){
-        //TODO
+        $post = $_POST;
+        if($post['login_name']==ADMIN_USER && $post['login_password']==ADMIN_PASS){
+            $_SESSION['admin'] = md5('xiejun');
+            header('Refresh:1; URL='.kernel::get_url().'/index.php/admin');
+            exit(self::admin_msg_box('登陆成功'));
+        }
+        header('Refresh:1; URL='.kernel::get_url().'/index.php/admin-login');
+        exit(self::admin_msg_box('请输入正确的用户名密码',false));
     }
 
     public function index(){
+        self::blog();
+    }
+
+    public function blog(){
         $db = kernel::dataFile();
         $blogId = $db->fetch('base','content.ids');
         $blog = array();
@@ -32,10 +54,7 @@ class controller_admin_baseController extends controller_baseController{
         }
         $this->view->set('blog',$blog);
         $this->view->display('admin/index.html');
-    }
-
-    public function blog(){
-        $this->view->display('admin/blog.html');
+//        $this->view->display('admin/blog.html');
     }
 
     public function blog_detail($id){
@@ -66,15 +85,33 @@ class controller_admin_baseController extends controller_baseController{
             $db->store('base', $markIds, $old_ids);
         }
         header('Refresh:1; URL='.kernel::get_url().'/index.php/admin');
-        exit('<div style="width:800px;
-    margin:0 auto;
-    padding:20px 10px;
-    background: lightseagreen;
-    font-size: 16px;
-    color: white;"><p>保存成功！(1秒后自动跳转)</p></div>');
+        exit(self::admin_msg_box('保存成功！(1秒后自动跳转)'));
     }
 
     public function blog_delete($id){
-        //
+        $db = kernel::dataFile();
+        if(!$id){
+            return;
+        }else{
+            $db->remove('content', $id);
+            $markIds = 'content.ids';
+            $old_ids = $db->fetch('base',$markIds);
+            foreach($old_ids as $key => $value){
+                if($value == $id || $value=='') continue;
+                $newIds[] = $value;
+            }
+            $db->store('base',$markIds,$newIds);
+            header('Refresh:1; URL='.kernel::get_url().'/index.php/admin-blog');
+            exit(self::admin_msg_box('删除成功！(1秒后自动跳转)'));
+        }
+    }
+
+    public static function admin_msg_box($msg, $type=true){
+        return '<div style="width:800px;
+    margin:0 auto;
+    padding:20px 10px;
+    background: '.($type?'lightseagreen':'lightcoral').';
+    font-size: 16px;
+    color: white;"><p>'.$msg.'</p></div>';
     }
 }
